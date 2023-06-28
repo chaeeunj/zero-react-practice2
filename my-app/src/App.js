8947636126b6317da487731ed371a5493827d367
chaeeunj <official2930@naver.com>
2023-06-29 04:52:42 +0900

    클릭하면 이동하는 Link 추가

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import MainPage from './components/MainPage';
 import TechPage from './components/TechPage';
 import BlogPage from './components/BlogPage';
