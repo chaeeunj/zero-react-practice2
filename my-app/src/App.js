98102a453f91d66ce4885036ffb4fdd866eb20fc
chaeeunj <official2930@naver.com>
2023-06-29 05:03:38 +0900

    Nested Routes 구성

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,18 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import MainPage from './components/MainPage';
 import TechPage from './components/TechPage';
 import BlogPage from './components/BlogPage';
+import JavascriptPage from './components/JavascriptPage';
+import ReactPage from './components/ReactPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={'/'} element={<MainPage />}></Route>
-        <Route path={'/tech'} element={<TechPage />}></Route>
+        <Route path={'/tech'} element={<TechPage />}>
+          <Route path="javascript" element={<JavascriptPage />}></Route>
+          <Route path="react" element={<ReactPage />}></Route>
+        </Route>
         <Route path={'/blog'} element={<BlogPage />}></Route>
       </Routes>
     </BrowserRouter>
