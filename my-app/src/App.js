2412920eb71257c98340951baea972c7945e000b
chaeeunj <official2930@naver.com>
2023-06-29 04:48:42 +0900

    기본적인 router 구성

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './components/MainPage';
+import TechPage from './components/TechPage';
+import BlogPage from './components/BlogPage';
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={<MainPage />}></Route>
+        <Route path={'/tech'} element={<TechPage />}></Route>
+        <Route path={'/blog'} element={<BlogPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
