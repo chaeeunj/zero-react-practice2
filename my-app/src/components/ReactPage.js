77e4179195944a358112e1030d914e653d5630d0
chaeeunj <official2930@naver.com>
2023-06-29 15:36:49 +0900

    react로 fetch API 불러오기

--- my-app/src/components/ReactPage.js
+++ my-app/src/components/ReactPage.js
@@ -1,36 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
 export default function ReactPage() {
   // api call -> react에 해당하는 글의 목록을 응답 받음.
 
-  const docs = [
-    {
-      id: 1,
-      title: 'React 공부를 시작하면서',
-      date: '05/01/2021',
-    },
-    {
-      id: 2,
-      title: 'CRA 없이 리액트 프로젝트 시작하기',
-      date: '12/01/2021',
-    },
-    {
-      id: 3,
-      title: '이제는 사용해보자 useMemo & useCallback',
-      date: '31/01/2021',
-    },
-    {
-      id: 4,
-      title: 'React 프로젝트에 Prettier 적용하기',
-      date: '15/02/2021',
-    },
-    {
-      id: 5,
-      title: 'React의 setState() 제대로 사용하기',
-      date: '28/02/2021',
-    },
-  ];
+  const [docs, setDocs] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+      console.log(res);
+      const result = await res.json();
+      return result;
+    }
+
+    fetchData().then((res) => {
+      setDocs(res);
+    });
+  }, []);
 
   return (
     <div>
