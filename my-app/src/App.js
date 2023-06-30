188c7b55212f29dced4fac573c164f301526b848
chaeeunj <official2930@naver.com>
2023-06-30 16:53:59 +0900

    Context를 사용하여 컴포넌트들에 데이터 제공하기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,20 +5,23 @@ import BlogPage from './components/BlogPage';
 import JavascriptPage from './components/JavascriptPage';
 import ReactPage from './components/ReactPage';
 import ReactDocPage from './components/ReactDocPage';
+import UserStore from './store/user';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={'/'} element={<MainPage />}></Route>
-        <Route path={'/tech'} element={<TechPage />}>
-          <Route path="javascript" element={<JavascriptPage />}></Route>
-          <Route path="react" element={<ReactPage />}></Route>
-          <Route path="react/:docId" element={<ReactDocPage />}></Route>
-        </Route>
-        <Route path={'/blog'} element={<BlogPage />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <UserStore>
+      <BrowserRouter>
+        <Routes>
+          <Route path={'/'} element={<MainPage />}></Route>
+          <Route path={'/tech'} element={<TechPage />}>
+            <Route path="javascript" element={<JavascriptPage />}></Route>
+            <Route path="react" element={<ReactPage />}></Route>
+            <Route path="react/:docId" element={<ReactDocPage />}></Route>
+          </Route>
+          <Route path={'/blog'} element={<BlogPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </UserStore>
   );
 }
 
---
+++ my-app/src/store/user.js
@@ -0,0 +1,16 @@
+import React, { createContext, useState } from 'react';
+
+export const UserContext = createContext();
+
+export default function UserStore(props) {
+  const [job, setJob] = useState('FE-developer');
+
+  const user = {
+    name: 'jce',
+    job,
+    changeJob: (updatedJob) => setJob(updatedJob),
+  };
+  return (
+    <UserContext.Provider value={user}>{props.children}</UserContext.Provider>
+  );
+}
